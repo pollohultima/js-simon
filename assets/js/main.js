5de95246359e71e29f67651eec7253d31fa1776c
pollohultima <matteo.polonio@gmail.com>
2021-10-27 15:26:37 +0200

    got the 5 prompt after 30 sec

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,15 +8,55 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 
 
-
+//creo l'array 
 let numeri = []
+
+//ciclo per generare 5 numeri random
 for (let i = 0; i < 5; i++) {
     const randoms = Math.floor((Math.random() * 100) + 1);
     console.log(randoms);
+
+    //Pusho i numeri random dentro l'array 'numeri'
     numeri.push(randoms)
 }
 
+//Visualizzo l'array di 5 numeri in console
+console.log(numeri);
 alert(numeri);
+
+
+
+let clock
+let seconds = 0;
+clock = setInterval(counter, 1000);
+
+
+function counter() {
+    if (seconds === 5) {
+        clearInterval(clock)
+
+        let userNum = []
+
+        for (let i = 0; i < 5; i++) {
+            const userNumbers = parseInt(prompt('Inserisci 5 numeri'));
+            console.log(userNumbers);
+
+            //Pusho i numeri random dentro l'array 'numeri'
+            userNum.push(userNumbers)
+        }
+
+        console.log(userNum);
+    } else {
+        seconds++
+        console.log(seconds);
+    }
+}
+
+
+
+
+
+
 
 
 
